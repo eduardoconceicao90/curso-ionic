74251925f55e3a2f58c1dc6b5b03fc0921c98ac0
Eduardo Souza de Almeida Conceição <eduardo.conceicao@setranspe.net>
2024-06-06 10:48:39 -0300

    Melhorando menu

--- curso-ionic6/src/app/app.component.ts
+++ curso-ionic6/src/app/app.component.ts
@@ -6,18 +6,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public appPages = [
+  public menuComponentCollection  = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Accordion', url: 'accordion', icon: 'dice' },
     { title: 'Action-Sheet', url: 'actionsheet', icon: 'dice' },
     { title: 'Alert', url: 'alert', icon: 'dice' },
     { title: 'Button', url: 'button', icon: 'dice' },
     { title: 'Card', url: 'card', icon: 'dice' },
-    { title: 'Datetime', url: 'datetime', icon: 'dice' },
     { title: 'Components - Tags', url: 'tags', icon: 'dice' },
     { title: 'Components - JS', url: 'js', icon: 'dice' },
+    { title: 'Datetime', url: 'datetime', icon: 'dice' },
+  ];
+
+  public menuNativeCollection = [
+    { title: 'Camera', url: 'camera', icon: 'dice' },
+    { title: 'Flash', url: 'flash', icon: 'dice' },
   ];
   
-
   constructor() {}
 }

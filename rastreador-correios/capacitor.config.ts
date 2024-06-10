26a29da216365cc32f5db5891612ee6b67397b41
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-06-10 09:50:29 -0300

    Criando projeto rastreador de encomendas

---
+++ rastreador-correios/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rastreador-correios',
+  webDir: 'www'
+};
+
+export default config;
